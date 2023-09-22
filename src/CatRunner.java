f15b5f125b3f1c3b73ada093a24408eda23f282e
BT_1N3_01 <sample@bths.edu>
2023-09-22 09:01:23 -0400

    initial commit

---
+++ src/CatRunner.java
@@ -0,0 +1,8 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Bob", 7, 10.8);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+    }
+}
