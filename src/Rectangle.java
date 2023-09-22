485e8a4d46c0ce04a1511f077f00e30ad933868e
BT_1N3_01 <sample@bths.edu>
2023-09-22 09:29:08 -0400

    commit3

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -17,6 +17,7 @@ public class Rectangle {
     public void printArea() {
         int area = length * width;
         System.out.println("My area is " + area);
+
     }
     public void printPerimeter() {
         int perimeter = 2 * length + 2 * width;
